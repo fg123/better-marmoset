c9b83c42cdac95dc5f37a4e7943d462e99d77339
Felix Guo <felixguoxiuping@gmail.com>
2018-11-17 18:49:22 -0500

    Fixed some release stuff

--- build/better-marmoset.js
+++ build/better-marmoset.js
@@ -1,43 +1,39 @@
-$(document).ready(function () {
-	$("tr").each(function (index) {
-		if (index == 0) { 
-			$(this).append(`<th>#</th>
+$(document).ready(function() {
+    $('tr').each(function(index) {
+        if (index == 0) {
+            $(this).append(`<th>#</th>
 				<th>submitted</th>
 				<th>public tests <br>score</th>
-				<th>detailed<br>test results</th>
-				<th>passed</th>
-				<th>failed</th>
+				<th>release tests score</th>
+				<th>detailed test results</th>
 				<th>Download</th>`);
-		}
-		else { 
-			var projUrl = $(this).find("td:nth-child(2) a").attr("href");
-			var tr = $(this);
-			jQuery.ajax({
-				url: projUrl,
-				success: function (data) {
-					var data = $(data).find("tr:nth-child(2)");
-					if (data.length) {
-						var submissionUrl = $(data).find("td:nth-child(4) a").attr("href");
-						jQuery.get(submissionUrl, function (submissionData) {
-							var passed = 0, failed = 0;
-							$(submissionData).find("tr td:nth-last-child(2)").each(function () { 
+        } else {
+            var projUrl = $(this)
+                .find('td:nth-child(2) a')
+                .attr('href');
+            var tr = $(this);
+            jQuery.ajax({
+                url: projUrl,
+                success: function(data) {
+                    var header = $(data).find('tr:nth-child(1)');
+                    var data = $(data).find('tr:nth-child(2)');
+                    if (header.html().indexOf('release tests') < 0) {
+                        $(data)
+                            .find('td:nth-child(3)')
+                            .after('<td>No Release Tests</td>');
+                    } else {
+                        $(data)
+                            .find('td:nth-child(5)')
+                            .remove();
+                    }
 
-								if ($(this).text().indexOf("passed") !== -1) {
-									passed++;
-								}
-								else { 
-									failed++;
-								}
-							});
-							$(data).find("td:nth-child(4)").after("<td>" + passed + "</td><td>" + failed + "</td>");
-							tr.append(data.html());
-						});
-					}
-					else { 
-						tr.append(`<td colspan="5"><b>No submissions yet!</b></td>`);
-					}
-				}
-			});
-		}
-	});
+                    if (data.length) {
+                        tr.append(data.html());
+                    } else {
+                        tr.append(`<td colspan="6"><b>No submissions yet!</b></td>`);
+                    }
+                }
+            });
+        }
+    });
 });
